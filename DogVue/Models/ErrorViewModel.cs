4882536da8b71f3da24ffdc9f6cfc200e119b3a4
GregoryValsaint <gvalsaint@live.fr>
2021-06-10 16:20:10 +0200

    Ajoutez des fichiers projet.

---
+++ DogVue/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DogVue.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
